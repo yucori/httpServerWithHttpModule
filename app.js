67ff471533198180755659efdbb947b3029eca9a
jang_jiwon <kooriangman@gmail.com>
2023-02-16 19:56:08 +0900

    [ADD] 유저 회원가입 엔드포인트 구현

---
+++ app.js
@@ -0,0 +1,64 @@
+const users = [
+  {
+    id: 1,
+    name: "Rebekah Johnson",
+    email: "Glover12345@gmail.com",
+    password: "123qwe",
+  },
+  {
+    id: 2,
+    name: "Fabian Predovic",
+    email: "Connell29@gmail.com",
+    password: "password",
+  },
+];
+
+const posts = [
+  {
+    id: 1,
+    title: "간단한 HTTP API 개발 시작!",
+    content: "Node.js에 내장되어 있는 http 모듈을 사용해서 HTTP server를 구현.",
+    userId: 1,
+  },
+  {
+    id: 2,
+    title: "HTTP의 특성",
+    content: "Request/Response와 Stateless!!",
+    userId: 1,
+  },
+];
+
+const http = require("http");
+const server = http.createServer();
+
+const httpRequestListener = (request, response) => {
+  if (request.method === "POST") {
+    if (request.url === "/users") {
+      let body = "";
+      // data가 stream 형태로 들어온다.
+      request.on("data", (data) => {
+        body += data;
+      });
+      request.on("end", () => {
+        const user = JSON.parse(body);
+
+        users.push({
+          id: user.id,
+          name: user.name,
+          email: user.email,
+          password: user.password,
+        });
+
+        response.writeHead(201, { "Content-Type": "application/json" }); // (4)
+        response.end(JSON.stringify({ users: users })); // (5)
+      });
+    }
+  }
+};
+
+server.on("request", httpRequestListener);
+
+server.listen(8000, "127.0.0.1", function () {
+  // (7)
+  console.log("Listening to requests on port 8000");
+});
